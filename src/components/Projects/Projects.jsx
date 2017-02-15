59cef692f83fc8258a14c90b9b75e12c538f98f5
Sumbhav Sethia <Sumbhav.Sethia@aexp.com>
2017-02-15 07:54:40 -0700

    Started filling out projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,19 +1,17 @@
 import React, {PropTypes} from 'react';
+import projects from './my-projects.json';
 
 export default class Projects extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = projects;
 	}
 
 	render() {
 		return (
-			<div>
-
+			<div className="body container">
+				{this.state.projects.map((project, index) => <ExperienceItem key={index} title={project.title} technologies={project.technologies} link={project.link} time={project.time} source={project.source} description={project.description} highlights={project.highlights}/>)}
 			</div>
 		);
 	}
 }
-
-Projects.PropTypes = {
-
-};
