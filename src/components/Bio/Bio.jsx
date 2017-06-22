d15fcc4c626398b0536be52683789d237575c437
Sumbhav <cheeto10x@yahoo.com>
2017-06-21 19:36:06 -0700

    Added RevolutionParts to experience, added bio picture, compressed images, added background

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bio from './bio.json';
+import biopic from '../../assets/biopic.jpg';
 
 export default class Bio extends React.Component {
 	constructor(props) {
@@ -11,6 +12,7 @@ export default class Bio extends React.Component {
 	render() {
 		return (
 			<div className="body container">
+				<img className="biopic" src={biopic} title="John Lennon Wall in Prague!"/>
 				{this.state.paragraphs.map((paragraph, index) =>
 						<p key={index}>{paragraph}</p>
 				)}
