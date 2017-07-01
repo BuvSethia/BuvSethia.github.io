e1d8d70958d57e3d580a8ac75133d1be53c54d78
Sumbhav <cheeto10x@yahoo.com>
2017-07-01 16:14:51 -0700

    Cleaned up site on mobile

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -5,7 +5,6 @@ import biopic from '../../assets/biopic.jpg';
 export default class Bio extends React.Component {
 	constructor(props) {
 		super(props);
-		console.log(bio);
 		this.state = bio;
 	}
 
